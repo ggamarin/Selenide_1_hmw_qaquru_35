965fdd43e90c7efd47d055b9b58e9f8bb12762a7
Grigoriy Gamarin <gam110191@gmail.com>
2025-04-23 23:07:05 +0300

    test name changed

--- src/test/java/FindCodeExampleForJunit5.java
+++ src/test/java/FindCodeExampleForJunit5.java
@@ -1,7 +1,6 @@
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.byText;
@@ -13,18 +12,18 @@ public class FindCodeExampleForJunit5 {
         Configuration.browserSize = "1920x1080";
         Configuration.pageLoadStrategy = "eager";
         Configuration.browser = "chrome";
-
     }
+
     @Test
-    void shouldFindCodeExample() {
+    void shouldFindCodeExampleTest() {
         open("https://github.com/");
         $("[placeholder='Search or jump to...']").click();
-        $( "#query-builder-test").setValue("selenide").pressEnter();
+        $("#query-builder-test").setValue("selenide").pressEnter();
         $(".Box-sc-g0xbh4-0.JcuiZ h3 a").shouldHave(text("selenide/selenide")).click();
         $("#repository-container-header").shouldHave(text("selenide / selenide"));
         $("#wiki-tab").shouldBe(visible).click();
         $("#wiki-body").shouldHave(text("Soft assertions"));
-        $("#wiki-body").$(byText("Soft assertions")) .click();
+        $("#wiki-body").$(byText("Soft assertions")).click();
         $("#wiki-body").shouldHave(text("@ExtendWith({SoftAssertsExtension.class})\n" +
                 "class Tests {\n" +
                 "  @Test\n" +
@@ -36,5 +35,5 @@ public class FindCodeExampleForJunit5 {
                 "    $(\"#second\").should(visible).click();\n" +
                 "  }\n" +
                 "}"));
-}
+    }
 }
